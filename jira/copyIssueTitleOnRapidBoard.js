640784c6fa206a3c0a5b6f2bd939d0632fe87e82
Yohei Kokubo <yhei.register@gmail.com>
2020-08-22 22:47:45 +0900

    アクティブなスプリントの画面にチケットタイトルコピーボタンを設置する #8

---
+++ jira/copyIssueTitleOnRapidBoard.js
@@ -0,0 +1,143 @@
+// ==UserScript==
+// @name         JIRA title copy button on board lists.
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add title copy button to issue list.
+// @author       Yohei Kokubo
+// @match        https://*.atlassian.net/secure/RapidBoard.jspa*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // ボタンのテンプレート定義
+  const buttonTemplate = {
+    tag: 'div',
+    label: 'コピー',
+    className: 'aui-button aui-button-primary js-title-copy-button',
+    css: `
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      font-size: 10px;
+    `
+  }
+
+  /**
+   * 課題一覧のコンテナを指定し コンテナ内の課題リンクにコピーボタンを配置する
+   *
+   * @param {Node} issueLinkContainer
+   * @return {void}
+   */
+  function addCopyButtonToContainer(issueLinkContainer) {
+    // 非同期でDOMが設定される
+    // DOMが設定されるまで3回まで繰り返して待つ
+    const loopMaxCount = 3;
+    let currentCount = 0;
+    const loopId = setInterval(function() {
+      currentCount++;
+      if (currentCount > loopMaxCount) {
+        clearInterval(loopId);
+        return;
+      }
+      const issues = issueLinkContainer.querySelectorAll('.js-issue');
+      if (issues.length > 0) {
+        clearInterval(loopId);
+        addCopyButton(issues);
+      }
+    }, 3000);
+  }
+
+  /**
+   * 課題リストの課題毎にコピーボタンを配置する
+   *
+   * @param {NodeList} issues
+   * @return {void}
+   */
+  function addCopyButton(issues) {
+    Array.prototype.forEach.call(issues, issue => {
+      // 課題毎にボタンを作成する
+      const buttonNode = createCopyButtonElement(
+        buttonTemplate.tag,
+        buttonTemplate.className,
+        buttonTemplate.css,
+        buttonTemplate.label
+      );
+
+      // タイトルとチケット番号を取得
+      const title = issue.querySelector('.ghx-summary').dataset.tooltip;
+      const key = issue.querySelector('.ghx-key').dataset.tooltip;
+
+      // ボタンにコピーボタンが押された時の処理を追加
+      buttonNode.onclick = function(){
+        // コピーする文字列を作成
+        const copiedString = `${key}_${title}`;
+
+        // コピー用のDOMを作り 選択状態にする
+        const domForClipboard = document.createElement('div');
+        domForClipboard.appendChild(document.createElement('pre')).textContent = copiedString;
+        domForClipboard.style.cssText = `
+          position: fixed;
+          left: -100%;
+        `;
+        document.body.appendChild(domForClipboard);
+        document.getSelection().selectAllChildren(domForClipboard);
+
+        // 選択したコピー用のDOMをコピーする
+        const result = document.execCommand('copy');
+
+        // コピー用のDOMを削除
+        document.body.removeChild(domForClipboard);
+
+        return result;
+      };
+      if (issue.querySelectorAll('.js-title-copy-button').length > 0) {
+        // コピーボタン設置済みであればskip
+        return;
+      }
+      issue.appendChild(buttonNode);
+    });
+  }
+
+  /**
+   * 指定のタグ、クラス、CSS、ラベルでボタンのNodeを作成する
+   *
+   * @param {string} tag
+   * @param {string} className
+   * @param {string} css
+   * @param {string} label
+   * @return {Node}
+   */
+  function createCopyButtonElement(tag, className, css, label) {
+    const buttonNode = document.createElement(tag);
+    buttonNode.className = className;
+    buttonNode.style.cssText = css;
+    buttonNode.innerHTML = label;
+    return buttonNode;
+  }
+
+  /**
+   * 初期化関数
+   *
+   * @return {void}
+   */
+  function initialize() {
+    const observer = new MutationObserver(function(mutations) {
+      // 課題一覧のコンテナを取得する
+      const issueLinkContainer = document.querySelector('.ghx-columns');
+      if (!issueLinkContainer) {
+        return;
+      }
+      addCopyButtonToContainer(issueLinkContainer);
+      const copyButton = issueLinkContainer.querySelectorAll('.js-title-copy-button');
+      if (copyButton.length > 0) {
+        observer.disconnect(); // コピーボタンを追加できたら監視解除
+      }
+    });
+    // DOMの変更を検知したら ロード
+    observer.observe(document, { childList: true, subtree: true });
+  }
+
+  initialize();
+})();
